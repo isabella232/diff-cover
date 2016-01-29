a885ea64a560289d1abe15675c5a4f9a75083798
Simon Weber <simon@venmo.com>
2016-01-29 11:57:30 -0500

    0.9.0b3

--- diff_cover/__init__.py
+++ diff_cover/__init__.py
@@ -1,3 +1,3 @@
-VERSION = '0.9.0b2'
+VERSION = '0.9.0b3'
 DESCRIPTION = 'Automatically find diff lines that need test coverage.'
 QUALITY_DESCRIPTION = 'Automatically find diff lines with quality violations.'
